b9f5cacbf3e21c7ae9889cc313e3a20ea12a4ce2
Pat Boyer <pboyer@gmail.com>
2018-12-10 21:22:50 -0500

    solutions for day 7

--- Day07.cs
+++ Day07.cs
@@ -69,20 +69,46 @@ namespace advent_of_code_2018
             return nodes.Values.ToList();
         }
 
+        private class Worker
+        {
+            public Node job;
+            public int duration;
+            private int _step;
+            private int _time;
+
+            public Worker(int p_step) 
+            {
+                job   = null;
+                _step = p_step;
+            }
+
+            public void AssignWork(Node p_job)
+            {
+                _time    = 0;
+                job      = p_job;
+                duration = (int)(job.name) - 64 + _step;
+            }
+
+            public Node DoWork()
+            {
+                _time++;
+                return (duration == _time) ? job : null;
+            }
+        }
+
         public void SolveA()
         {
             List<Node> nodes = ParseFile("07_input.txt");
             List<Node> done  = new List<Node>();
             List<Node> todo  = nodes.Where(n => n.parents.Count() == 0).ToList();
+            List<Node> ready = new List<Node>();
 
             while (done.Count() < nodes.Count())
             {
-                foreach (Node t in todo)
-                {
-                    t.ready = (t.parents.Where(p => !done.Contains(p)).Count() == 0);
-                }
+                Node job = todo.Where(n => (n.parents.Where(p => !done.Contains(p)).Count()) == 0)
+                               .OrderBy(n => n)
+                               .First();
 
-                Node job = todo.Where(n => n.ready).OrderBy(n => n).First();
                 todo.Remove(job);
                 done.Add(job);
                 todo = todo.Union(job.children).ToList();
@@ -95,6 +121,53 @@ namespace advent_of_code_2018
         public void SolveB()
         {
             List<Node> nodes = ParseFile("07_input.txt");
+            List<Node> done  = new List<Node>();
+            List<Node> todo  = nodes.Where(n => n.parents.Count() == 0).ToList();
+            List<Node> ready = new List<Node>();
+            List<Worker> workers = new List<Worker>();
+            int duration = 0;
+            int step     = 60;
+
+            for (int i=0; i<5; i++)
+            {
+                workers.Add(new Worker(step));
+            }
+
+            while (done.Count() < nodes.Count())
+            {
+                duration++;
+
+                // Assign work
+                ready = todo.Where(n => (n.parents.Where(p => !done.Contains(p)).Count()) == 0)
+                            .OrderBy(n => n)
+                            .ToList();
+
+                foreach (Worker w in workers)
+                {
+                    if ( (ready.Count() > 0) && (w.job == null) )
+                    {
+                        Node job = ready.First();
+                        ready.Remove(job);
+                        todo.Remove(job);
+                        w.AssignWork(job);
+                    }
+                }
+
+                // do some work 
+                foreach (Worker w in workers)
+                {
+                    Node result = w.DoWork();
+                    if (result != null)
+                    {
+                        done.Add(result);
+                        todo = todo.Union(result.children).ToList();
+                        w.job = null;
+                    }
+                }
+            }
+
+            string code = string.Join("", done.Select(n => n.name));
+            Console.WriteLine("Day 07 B: " + code + " (" + duration + ")");
         }
     }
 }
