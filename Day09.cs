e5cf4f83773b299f54f6fe5df25e14333c598f26
Patrick Boyer <patrick.boyer@msj.org>
2018-12-19 13:19:53 -0500

    solution for day 9 part 2

--- Day09.cs
+++ Day09.cs
@@ -8,7 +8,7 @@ namespace advent_of_code_2018
 {
     class Day09
     {
-        public int Solve(int numPlayers, int lastMarble)
+        public int SolveByArray(int numPlayers, int lastMarble)
         {
             List<int> marbles = new List<int>() { 0 };
             List<int> players = new List<int>();
@@ -59,17 +59,129 @@ namespace advent_of_code_2018
 
         public void SolveA()
         {
-            int numPlayers = 416;
-            int lastMarble = 71617;
-            Console.WriteLine("Day 09 A: " + Solve(numPlayers, lastMarble));  //= 436720
+            int numPlayers = 9;//416;
+            int lastMarble = 32; //71617;
+            Console.WriteLine("Day 09 A: " + SolveByArray(numPlayers, lastMarble));  //= 436720
+        }
 
+        class Node
+        {
+            public Node Prev;
+            public Node Next;
+            public uint Data;
+
+            public Node(uint p_data)
+            {
+                Data = p_data;
+                Prev = null;
+                Next = null;
+            }
+        }
+
+        public class MarbleCircle 
+        {
+            private Node _current;
+            private Node _first;
+            private Node _last;
+
+            public MarbleCircle()
+            {
+                _first = _last = _current = new Node(0);
+            }
+
+            public void AddAfter(uint p_data)
+            {
+                Node n = new Node(p_data);
+                n.Next = _current.Next;
+                n.Prev = _current;
+                _current.Next = n;
+                _current = n;
+
+                if (_current.Next == null)
+                {
+                    _last = _current;
+                }
+                else
+                {
+                    _current.Next.Prev = n;
+                }
+            }
+
+            public uint GetData()
+            {
+                return _current.Data;
+            }
+
+            public void MoveBackward(int distance)
+            {
+                for (int i=0; i<distance; i++)
+                    _current = (_current == _first) ? _last : _current.Prev;
+            }
+
+            public void MoveForward(int distance)
+            {
+                for (int i=0; i<distance; i++)
+                    _current = (_current == _last) ? _first : _current.Next;
+            }
+
+            public void Remove()
+            {
+                if (_current == _last)
+                {
+                    _last = _current.Prev;
+                    _current = _first;
+                }
+                  
+                else if (_current == _first)
+                {
+                    _current = _first = _current.Next;
+                }
+                else 
+                {
+                    _current.Next.Prev = _current.Prev;
+                    _current.Prev.Next = _current.Next;
+                    _current = _current.Next;
+                }
+            }
+        }
+
+        public uint SolveByDoubleLinkedList(int numPlayers, uint lastMarble)
+        {
+            MarbleCircle marbles = new MarbleCircle();
+            uint currentMarble = 1;
+
+            List<uint> players = new List<uint>();
+            for (int i=0; i<numPlayers; i++)
+            {
+                players.Add(0);
+            }
+
+            while (currentMarble <= lastMarble)
+            {
+                if (currentMarble % 23 == 0)
+                {
+                    marbles.MoveBackward(7);                    
+                    int playerIdx = (int)(currentMarble % numPlayers);
+                    players[playerIdx] += marbles.GetData() + currentMarble;
+                    marbles.Remove();
+                }
+                else
+                {
+                    marbles.MoveForward(1);
+                    marbles.AddAfter(currentMarble);
+                }
+
+                currentMarble++;
+            }         
+
+            return players.Max();   
         }
 
         public void SolveB()
         {
-            int numPlayers    = 416;
-            int lastMarble    = 71617 * 100;
-            Console.WriteLine("Day 09 B: " + Solve(numPlayers, lastMarble));  //= 
+            int numPlayers  = 416;
+            uint lastMarble = 71617 * 100;
+            Console.WriteLine("Day 09 B: " + SolveByDoubleLinkedList(numPlayers, lastMarble));  //= 3527845091
         }
     }
 }
